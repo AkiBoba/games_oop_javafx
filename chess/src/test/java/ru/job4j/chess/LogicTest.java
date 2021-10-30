a301e6efc1ed5711006eb7bde0d83e985df24c7d
Vladimir Likhachev <lettervl@ya.ru>
2021-10-30 20:54:57 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -38,8 +38,8 @@ public class LogicTest {
             blackBisop.way(Cell.G3);
     }
 
-    @Test(expected = Exception.class)
-    public void noFreeTest() throws OccupiedCellException, FigureNotFoundException {
+    @Test(expected = OccupiedCellException.class)
+    public void noFreeTest() throws OccupiedCellException, FigureNotFoundException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new PawnBlack(Cell.A7));
         logic.add(new PawnBlack(Cell.B7));
@@ -57,6 +57,49 @@ public class LogicTest {
         logic.add(new BishopBlack(Cell.F8));
         logic.add(new KnightBlack(Cell.G8));
         logic.add(new RookBlack(Cell.H8));
-        logic.move(Cell.C8, Cell.A5);
+        logic.move(Cell.C8, Cell.A6);
+    }
+
+    @Test(expected = FigureNotFoundException.class)
+    public void noFoundTest() throws OccupiedCellException, FigureNotFoundException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new PawnBlack(Cell.A7));
+        logic.add(new PawnBlack(Cell.B7));
+        logic.add(new PawnBlack(Cell.C7));
+        logic.add(new PawnBlack(Cell.D7));
+        logic.add(new PawnBlack(Cell.E7));
+        logic.add(new PawnBlack(Cell.F7));
+        logic.add(new PawnBlack(Cell.G7));
+        logic.add(new PawnBlack(Cell.H7));
+        logic.add(new RookBlack(Cell.A8));
+        logic.add(new KnightBlack(Cell.B8));
+        logic.add(new QueenBlack(Cell.D8));
+        logic.add(new KingBlack(Cell.E8));
+        logic.add(new BishopBlack(Cell.F8));
+        logic.add(new KnightBlack(Cell.G8));
+        logic.add(new RookBlack(Cell.H8));
+        logic.move(Cell.C8, Cell.A6);
+    }
+
+    @Test(expected = ImpossibleMoveException.class)
+    public void noMoveTest() throws OccupiedCellException, FigureNotFoundException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new PawnBlack(Cell.A7));
+        logic.add(new PawnBlack(Cell.B7));
+        logic.add(new PawnBlack(Cell.C7));
+        logic.add(new PawnBlack(Cell.D7));
+        logic.add(new PawnBlack(Cell.E7));
+        logic.add(new PawnBlack(Cell.F7));
+        logic.add(new PawnBlack(Cell.G7));
+        logic.add(new PawnBlack(Cell.H7));
+        logic.add(new RookBlack(Cell.A8));
+        logic.add(new KnightBlack(Cell.B8));
+        logic.add(new BishopBlack(Cell.C8));
+        logic.add(new QueenBlack(Cell.D8));
+        logic.add(new KingBlack(Cell.E8));
+        logic.add(new BishopBlack(Cell.F8));
+        logic.add(new KnightBlack(Cell.G8));
+        logic.add(new RookBlack(Cell.H8));
+        logic.move(Cell.C8, Cell.A4);
     }
 }
